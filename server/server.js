1015840d5580d60c7b38b5532449c27039ccc8a9
Jason Chen <jason.ccfu@gmail.com>
2023-04-04 20:59:56 -0700

    create user controllers and api routes

--- server/server.js
+++ server/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
-// const routes = require('./routes');
+const routes = require('./routes');
 
 const app = express();
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3003;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,7 +14,7 @@ app.use(express.json());
 //   app.use(express.static(path.join(__dirname, '../client/build')));
 // }
 
-// app.use(routes);
+app.use(routes);
 
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
