1dc17e688222d4141f61ac82b24e4ef7e1ca1724
Josh <joshuaelitate@gmail.com>
2023-04-06 20:18:54 -0700

    small deletion

--- server/routes/api/user-routes.js
+++ server/routes/api/user-routes.js
@@ -7,14 +7,7 @@ const {
 // import middleware
 const { authMiddleware } = require('../../utils/auth');
 
-// put authMiddleware anywhere we need to send a token for verification of user
 router.route('/').post(createUser);
 router.route('/:userId').get(getSingleUser);
-
-// router.route('/login').post(login);
-
-// router.route('/me').get(authMiddleware, getSingleUser);
-
-// router.route('/books/:bookId').delete(authMiddleware, deleteBook);
 
 module.exports = router;
