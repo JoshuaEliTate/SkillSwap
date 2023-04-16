ef478a3c5c235a23e1f53d4e4a2c586d5268e014
Josh <joshuaelitate@gmail.com>
2023-04-16 14:31:50 -0700

    pushing to feature

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,7 @@ const { User, Skill, Category, Session } = require('../models');
 const { signToken } = require('../utils/auth');
 const { findById } = require('../models/Category');
 
-
+//hello
 const resolvers = {
   Query: {
     categories: async () => Category.find(),
