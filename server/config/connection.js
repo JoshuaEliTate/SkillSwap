fb834fd06a90bb9cc49eec8ee2d7f469d78ab653
Josh <joshuaelitate@gmail.com>
2023-04-17 22:05:25 -0700

    mongodb finishing touches

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/skillswap'
+  process.env.MONGO_URI || 'mongodb://127.0.0.1:27017/skillswap'
 );
 
 module.exports = mongoose.connection;
